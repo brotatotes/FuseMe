003f5f37a060a9e50745d6da7e21e197875b9345
softkaiser <austinchambers1@gmail.com>
2017-02-21 21:11:14 -0600

    did more flask shit, check issues

--- WebApp/hello.py
+++ WebApp/hello.py
@@ -1,10 +1,11 @@
 
+from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return Flask.render_template('test.html')
+    return render_template('test.html')
 
 if __name__ == "__main__":
     app.run()
