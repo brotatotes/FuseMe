d594361cab9e545f7ce0fd9d4260744afa8819ce
Junhao Li <junhaoli2019@u.northwestern.edu>
2017-02-26 22:32:04 -0600

    Update to spectral envelope function

--- cross-synthesis/fuse.py
+++ cross-synthesis/fuse.py
@@ -8,7 +8,7 @@ def fuse(modulator, carrier):
 	Takes in modulator and carrier signals and returns a fused signal. 
 	Typically, a modulator is a voice, and the carrier is any spectrally rich sound.
 
-	- input -
+	- inpu
 	modulator: np array
 	carrier: np array
 
@@ -20,9 +20,39 @@ def fuse(modulator, carrier):
 	carrier_stft = librosa.core.stft(carrier)
 
 
-def get_spectral_envelope(frame, sr):
+def get_spectral_envelope(frame):
+	frame = np.log(np.abs(frame))
 	fs = len(frame)
-	winframe = hamming(fs) * frame
-	frame_fft = fft(winframe, 4*fs)
-	log_fft = 20*np.log(np.abs(frame_fft)
-	real_cepstrum = np.real(ifft(log_fft))
+	real_cepstrum = np.real(ifft(frame))
+	nw = 20
+	high_low_window = hamming(fs)[fs - nw:]
+	low_high_window = hamming(fs)[:nw]
+	wzp = np.concatenate((high_low_window, np.zeros(fs - (2 * nw)), low_high_window))
+	wrcep = wzp * real_cepstrum
+	rcepenv = fft(wrcep)
+	rcepenvp = np.real(rcepenv)
+	rcepenvp = rcepenvp - np.mean(rcepenvp)
+
+
+	# Direct translation of code from textbook
+
+	# fs = len(sample)
+	# w = hamming(fs)
+	# winspeech = w * sample
+	# Nfft = 4*fs
+	# sspec = fft(winspeech, Nfft)
+	# dbsspecfull = 20*np.log(np.abs(sspec))
+	# rcep = ifft(dbsspecfull)
+	# rcep = np.real(rcep)
+	# period = 41 # what is period ??
+	# nspec = Nfft/2+1
+	# nw = 2*period - 4
+	# if nw % 2 == 0:
+	# 	nw -= 1
+	# w = boxcar(nw)
+	# wzp = np.concatenate((w[(nw+1)/2:], np.zeros(Nfft - nw), w[:(nw+1)/2]))
+	# wrcep = wzp * rcep
+	# rcepenv = fft(wrcep)
+	# rcepenvp = np.real(rcepenvp[:nspec])
+	# rcepenvp -= np.mean(rcepenvp)
+
