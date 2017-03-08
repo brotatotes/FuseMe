f9515d88b1efa8f03a9c5f25c614b23f4c180eb2
softkaiser <austinchambers1@gmail.com>
2017-03-07 19:27:04 -0600

    fixed BULL SHIT INDENTATION BUG #FUCKPYTHON

--- WebApp/hello.py
+++ WebApp/hello.py
@@ -7,15 +7,15 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
-        f = request.files['file']
-        f.save(os.path.join('static', secure_filename(f.filename)))
-	return render_template('test.html')    
+	if request.method == 'POST':
+		f = request.files['file']
+		f.save(os.path.join('static', secure_filename(f.filename)))
+	return render_template('test.html')
+	    
 
 @app.route("/playback", methods=['GET', 'POST'])
 def playback():
 	return render_template('playback.html')   
-
 
 if __name__ == "__main__":
     app.run()
