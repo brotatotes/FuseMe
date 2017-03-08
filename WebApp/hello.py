28a228de0f9254e1a0a9ed63df775d85dde5710a
softkaiser <austinchambers1@gmail.com>
2017-03-07 19:08:18 -0600

    uploading a playback updates for webapp

--- WebApp/hello.py
+++ WebApp/hello.py
@@ -1,11 +1,21 @@
-
-from flask import Flask
+from flask import Flask, request, redirect, url_for
 from flask import render_template
+from werkzeug import secure_filename
+import os
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return render_template('test.html')
+@app.route("/", methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(os.path.join('static', secure_filename(f.filename)))
+	return render_template('test.html')    
+
+@app.route("/playback", methods=['GET', 'POST'])
+def playback():
+	return render_template('playback.html')   
+
 
 if __name__ == "__main__":
     app.run()
