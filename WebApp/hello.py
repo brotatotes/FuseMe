37efa1da17100a7ae07d1522d11251dbfd1a4f43
softkaiser <austinchambers1@gmail.com>
2017-03-12 17:29:58 -0500

    almost functional
    Former-commit-id: 994627abd03cac2bea1a8fdf66bd600ed664fc1d

--- WebApp/hello.py
+++ WebApp/hello.py
@@ -1,9 +1,43 @@
 from flask import Flask, request, redirect, url_for
 from flask import render_template
 from werkzeug import secure_filename
+import librosa
 import os
 
 app = Flask(__name__)
+
+def wavwrite(filepath, data, sr, norm=True, dtype='int16',):
+    if norm:
+        data /= np.max(np.abs(data))
+    data = data * np.iinfo(dtype).max
+    data = data.astype(dtype)
+    sp.io.wavfile.write(filepath, sr, data)
+
+def cross_synthesize(mod, car):
+    # takes two 1D np arrays
+    # mod - the modulator signal
+    # car - the carrier signal
+    #
+    # returns cross synthesis of the two signals
+    # 
+    n_fft = 4096
+    hop_length = 1024 
+    
+    smaller_length = min(len(mod), len(car))
+
+    mod = mod[:smaller_length]
+    car = car[:smaller_length]
+
+    modstft = librosa.core.stft(mod, n_fft, hop_length)
+    carstft = librosa.core.stft(car, n_fft, hop_length)
+
+    car_env = hilbert(np.abs(carstft), axis=0)
+    flat_car = carstft/(car_env+1e-20)
+    mod_env = np.abs(hilbert(np.abs(modstft), axis=0))
+    cross = flat_car * mod_env
+    result = librosa.core.istft(cross, hop_length)
+
+    return result
 
 @app.route("/", methods=['GET', 'POST'])
 def upload_file():
@@ -11,11 +45,32 @@ def upload_file():
 		f = request.files['file']
 		f.filename = 'uploaded.mp3'
 		f.save(os.path.join('static', secure_filename(f.filename)))
-	return render_template('test.html')
-	    
+		return '', 204
+	else:
+		return render_template('test.html')
+
+@app.route("/file/<id>", methods=["POST"])
+def file(id):
+	if request.method == "POST":
+		if id == '1':
+			f = request.files['file']
+			f.filename = 'carrier.wav'
+			f.save(os.path.join('static', secure_filename(f.filename)))
+			return '', 204
+		else:
+			f = request.files['file']
+			f.filename = 'modulator.wav'
+			f.save(os.path.join('static', secure_filename(f.filename)))
+			return '', 204
+	else:
+		return render_template('test.html')
 
 @app.route("/playback", methods=['GET', 'POST'])
 def playback():
+	modulator, sr = librosa.load('modulator.wav', 44100)
+	carrier, sr = librosa.load('carrier.wav', 44100)
+	fusion = cross_synthesize(modulator, carrier)
+	wavwrite('fusion.wav', fusion, 44100, norm=True, dtype='int16')
 	return render_template('playback.html')   
 
 @app.route("/about", methods=['GET','POST'])
