974b0db89581c9a0fbcf7ac7891d194cdd7827d4
brotatotes <hao.awesome@gmail.com>
2017-03-14 20:53:09 -0500

    virtualenv
    Former-commit-id: 497bce9ed070d332f2f65c0386f1b696455cb477

--- FuseMe.py
+++ FuseMe.py
@@ -79,5 +79,7 @@ def reset():
 def about():
 	return render_template('about.html')
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+     app.debug = True
+     port = int(os.environ.get("PORT", 5000))
+     app.run(host='0.0.0.0', port=port)
